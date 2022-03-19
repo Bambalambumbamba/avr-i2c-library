a4adaca6bb61dcc0c25a9a5c847bbd8310028b43
Samuele <vannoni.samuele@gmail.com>
2022-03-19 17:45:33 +0100

    corrected pins for at90can

--- twi/twi_master.h
+++ twi/twi_master.h
@@ -16,8 +16,8 @@
 #define DEBUG_LOG			0
 #define SUCCESS				0
 
-#define TW_SCL_PIN			LOPORTIO
-#define TW_SDA_PIN			PORTC4
+#define TW_SCL_PIN			PORTD0
+#define TW_SDA_PIN			PORTD1
 
 #define TW_SLA_W(ADDR)		((ADDR << 1) | TW_WRITE)
 #define TW_SLA_R(ADDR)		((ADDR << 1) | TW_READ)
